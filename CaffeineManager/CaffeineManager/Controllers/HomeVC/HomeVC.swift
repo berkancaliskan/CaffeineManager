e97f9a15d3a88c5426724bf429e8faff12629e01
Berkan Çalışkan <berkan.caliskan@arcelik.com>
2023-11-18 22:46:36 +0300

    initial commit

---
+++ CaffeineManager/CaffeineManager/Controllers/HomeVC/HomeVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeVC.swift
+//  CaffeineManager
+//
+//  Created by Berkan Çalışkan on 18.11.2023.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
